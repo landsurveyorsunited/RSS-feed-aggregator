5fa740695b9e4864c316df120730f48d34f2feae
Justin Farrow <landsurveyorsunited@gmail.com>
2022-11-01 13:17:20 -0400

    Update firebase.js
    Signed-off-by: Justin Farrow <landsurveyorsunited@gmail.com>

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,11 +2,11 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAa4N_qFVcS43On2X-YflEzwAudUHrn3QE",
-  authDomain: "rss-feed-30b89.firebaseapp.com",
-  projectId: "rss-feed-30b89",
-  storageBucket: "rss-feed-30b89.appspot.com",
-  appId: "1:35829901191:web:0f120b2a96f55fb0a8d522",
+  apiKey: "AIzaSyAKEFBnHme7nqqj1zJGDDfU1CE3bX2prwM",
+  authDomain: "land-surveyors-united.firebaseapp.com",
+  projectId: "land-surveyors-united",
+  storageBucket: "land-surveyors-united.appspot.com",
+  appId: "1:983786770163:web:eaf17bbb9391805f03b186",
 };
 
 firebase.initializeApp(firebaseConfig);
